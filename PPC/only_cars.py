f90de441be9df70f1c4492f6ffc3731f29364644
Angeliano Sousa <angeliano.sousa@gmail.com>
2023-04-16 10:48:25 -0300

    feat: finalizando a primeira parte do trabalho

--- PPC/only_cars.py
+++ PPC/only_cars.py
@@ -14,21 +14,26 @@
         ** caso não carro espera lock.wait()
 '''
 
-from threading import Thread, Condition, current_thread
+from threading import Thread, Condition, current_thread, enumerate
 from time import sleep, time
-from random import randint, choice
+from random import randint
 
 # Consts variables
 N_CARS               = 100 # Number of cars
-P_THROUGH            = 5  # Time pass through the bridge
+P_THROUGH            = 5   # Time pass through the bridge
 T_SPACE_BETWEEN_CARS = 2   # Time space between the cars
 
 # Global variables
 lock       = Condition()
 directions = ['right', 'left']
 bridge     = []
+cars       = []
+
+# Time variables
 time_start = 0
 time_end   = 0
+total_time_left  = 0
+total_time_right = 0
 
 class Car:
   def __init__(self, direction):
@@ -36,10 +41,18 @@ class Car:
     self.time_for_comming = randint(1, 3) # Tempo de travessia do carro
 
   def crossing_process(self):
-    # time_start = time()
+    global total_time_left
+    global total_time_right
+
+    time_start = time()
     self.to_enter_the_bridge()
     self.to_out_the_bridge()
-    # time_end = time()
+    time_end = time()
+
+    if self.direction == 'left':
+      total_time_left += time_end - time_start
+    else:
+      total_time_right += time_end - time_start
   
   # Function to start get in the bridge process
   def to_enter_the_bridge(self):
@@ -51,7 +64,7 @@ class Car:
         self.speed_up()
       else:
         while bridge[-1].direction != self.direction:
-          print(f'Car {self.direction} in opposite directions, putting on hold...')
+          print(f'#== Car {self.direction} in opposite directions, putting on hold... ==#')
           lock.wait()
 
           self.speed_up()
@@ -75,9 +88,35 @@ class Car:
     lock.notify_all()
     lock.release()
 
-if __name__ == "__main__":
-
+def creating_cars():
   for i in range(0, N_CARS):
     new_car_thread = Thread(name=f'Car {i}', target=Car(directions[i%2]).crossing_process, args=())
+    cars.append(new_car_thread)
+  
+def starting_cars():
+  for i in range(0, N_CARS):
     sleep(T_SPACE_BETWEEN_CARS)
-    new_car_thread.start()
+    cars[i].start()
+  
+  for i in range(0, N_CARS):
+    cars[i].join()
+
+def printing_results():
+  print(f'Total time for left cars: {total_time_left}')
+  print(f'Total time for right cars: {total_time_right}')
+  print(f'Media time left: {total_time_left/N_CARS}')
+  print(f'Media time right: {total_time_right/N_CARS}')
+
+if __name__ == "__main__":
+
+  creating_cars()
+  starting_cars()
+  printing_results()
+
+  # print(len(enumerate()))
+  # while len(enumerate()) > 0:
+    # printing_results()
+    # print(f'Total time for left cars: {total_time_left}')
+    # print(f'Total time for right cars: {total_time_right}')
+    # print(f'Media time left: {total_time_left/N_CARS}')
+    # print(f'Media time right: {total_time_right/N_CARS}')
