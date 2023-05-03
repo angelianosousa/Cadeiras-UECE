e0db68f6adef1a201bd9acdafe4565ed49e01702
Angeliano Sousa <angeliano.sousa@gmail.com>
2023-05-03 14:55:24 -0300

    finally a code that works properly!!

--- PPC/base_problem.py
+++ PPC/base_problem.py
@@ -1,121 +1,82 @@
-'''  
-  # Fluxo:
-    - Carro chega até o inicio da ponte
-    - Checa se a ponte está vazia
-      + caso sim acelera e lock.release() e bridge.pop()
-      + caso não
-      + Confere se o último carro tem a mesma direção que ele
-        ** caso sim carro acelera e lock.release() e libera bridge.pop()
-        ** caso não carro espera lock.wait()
-'''
-
-# TODO | Adicionar uma metodo a classe Bridges para adicionar e remover carros de forma atômica
-
-from threading import Thread, Condition, get_native_id
-from time import sleep, time
-from random import randint
-
-# Consts variables
-N_CARS               = 100 # Number of cars
-P_THROUGH            = 5   # Time pass through the bridge
-T_SPACE_BETWEEN_CARS = 1   # Time space between the cars
-
-class Bridges():
-  def __init__(self):
-    self.lane = []
+from threading import Thread, Condition, current_thread, get_native_id
+from time import time, sleep
+from random import randint, choice
 
 class Cars:
-  def __init__(self, direction):
-    self.direction        = direction
-    self.time_for_comming = randint(1, 3) # Tempo de travessia do carro
+  def __init__(self, direction, number):
+    self.direction   = direction
+    self.number      = number
+    self.time_arrive = randint(1, 3)
 
-  def crossing_process(self):
-    global cars_left
-    global cars_right
+class Bridges:
+  def __init__(self):
+    self.lane       = []
+    self.cars_enter = 0
+    self.cars_out   = 0
 
-    self.to_enter_the_bridge()
-    self.to_out_the_bridge()
+def leave_bridge(condition):
+  global bridge
 
-    if self.direction == 'left':
-      cars_left += 1
-    else:
-      cars_right += 1
-  
-  # Function to start get in the bridge process - Productor
-  def to_enter_the_bridge(self):
+  while bridge.cars_out < N_CARS:
 
-    with lock:
-      lock.acquire()
+    with condition:
+      condition.acquire()
 
-      if len(bridge.lane) == 0:
-        self.speed_up()
-      else:
-        if bridge.lane[-1].direction != self.direction and bridge.lane[-1].time_for_comming > self.time_for_comming:
-          print(f'# Car {get_native_id()} on {self.direction} putting on hold...')
-          print(f'Car {get_native_id()} need {self.time_for_comming} seconds to across...')
-          lock.wait()
+      while len(bridge.lane) == 0:
+        print(f'Bridge empty, stay on hold...')
+        condition.wait()
 
-        sleep(1)
-        self.speed_up()
-  
-  # Function to start get out the bridge process - Consumer
-  def to_out_the_bridge(self):
+      car = bridge.lane[0]
 
-    with lock:
-      lock.acquire()
+      sleep(TIME_PASS)
+      bridge.lane.pop(0)
+      bridge.cars_out += 1
 
-      sleep(self.time_for_comming)
-      bridge.lane.pop(0) # Car finally out
+      print(f'{current_thread().name} take off: Car {car.number} on #{car.direction}')
+      print(20*'-=-')
+      condition.notify_all()
+      condition.release()
 
-      print(f'Car {get_native_id()} was left now...')
-  
-  def speed_up(self):
-    global cars_count
+def enter_bridge(condition):
+  global bridge
 
-    cars_count += 1
-    sleep(1)
-    print(f'================================= {cars_count} ==============================')
+  car_number = 1
+    
+  while bridge.cars_out < N_CARS:
+    car = Cars(choice(['left', 'right']), car_number)
+    car_number += 1
 
-    print(f'Car {get_native_id()} on the bridge, can leave in *{self.time_for_comming} seconds* on the #{self.direction}')
-    bridge.lane.append(self) # Car get in
-    lock.notify()
-    lock.release()
+    with condition:
+      condition.acquire()
 
-def creating_cars():
-  for i in range(0, N_CARS):
-    new_car = Thread(name=f'Car {i}', target=Cars(directions[i%2]).crossing_process, args=())
-    cars.append(new_car)
-  
-def starting_cars():
-  for i in range(0, N_CARS):
-    sleep(T_SPACE_BETWEEN_CARS)
-    cars[i].start()
-  
-  for i in range(0, N_CARS):
-    cars[i].join()
+      # Check if the cars are in opposite sides
+      if len(bridge.lane) > 0 and bridge.lane[0].direction == car.direction:
+        print(f'Car {bridge.lane[0].number} has the same direction than Car {car.number}')
+        print('Putting on hold...')
+        condition.wait()
 
-def priting_results():
-  print(f'Nº cars left: {cars_left}')
-  print(f'Nº cars right: {cars_right}')
-  # print(f'Minimal time for wait: {min_wait}')
+      sleep(car.time_arrive)
+      bridge.lane.append(car)
+      bridge.cars_enter += 1
 
-if __name__ == "__main__":
-  # Count cars variables
-  cars_left  = 0
-  cars_right = 0
+      print(20*'-=-')
+      print(f'{current_thread().name} put Car: {car.number} on #{car.direction} - {car.time_arrive} seconds to arrive')
+      condition.notify_all()
+      condition.release()
 
-  # Time variables
-  min_wait = 0
-  max_wait = 0
-  # max_time_on_bridge = 0
+if __name__ == '__main__':
+  # Const variables
+  N_CARS = 100  # Cars numbers
+  TIME_PASS = 5 # Time for a car pass thought bridge
 
-  # Global variables
-  lock       = Condition()
-  bridge     = Bridges()
-  directions = ['right', 'left']
-  cars       = []
-  cars_count = 0
+  bridge = Bridges()
+  condition = Condition() # Lock variable
 
-  creating_cars()
-  starting_cars()
-  priting_results()
+  cs1 = Thread(name='Leave bridge', target=leave_bridge, args=(condition, ))  # Start process to leave the bridge
+  pr1 = Thread(name='Enter bridge', target=enter_bridge, args=(condition, ))  # Start process to enter the bridge
+
+  cs1.start()
+  sleep(2)
+
+  pr1.start()
+  sleep(2)
